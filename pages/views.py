0530656743932a10728c2d1432f06e865f77aace
Romain BOBOE <rboboe@gmail.com>
2020-04-24 11:14:30 -0400

    adding all the files for the static

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# pages/views.py
+from django.views.generic import TemplateView
+
+def index(request):
+
+  return render(request, 'index.html')
+
