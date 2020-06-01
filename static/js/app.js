cf2f81a8ff5442f0c4fbb1dd71c3579a940194ae
Romain BOBOE <rboboe@gmail.com>
2020-06-01 12:15:42 -0400

    Adding more image and styling home page for smaller devices

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,14 @@
 function openNav() {
+  var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  if (screenWidth < 768) {
+    document.getElementById("mySidenav").style.height = "50vh";
+    document.getElementById("main").style.marginTop = "50vh";
+  } else {
   document.getElementById("mySidenav").style.width = "50vw";
   document.getElementById("main").style.marginLeft = "50vw";
+  document.documentElement.style.overflow = "hidden";
+  document.body.style.overflow = "hidden";
+   }
   document.getElementById("path").style.stroke = "#FFF";
 }
 
@@ -10,5 +18,7 @@ function closeNav() {
 
   document.getElementById("mainNav").className.includes("navbar-dark") ?
   document.getElementById("path").style.stroke = "rgba(255, 255, 255, 0.8)" :
-  document.getElementById("path").style.stroke = "rgba(0, 0, 0, 0.8)"
+  document.getElementById("path").style.stroke = "rgba(0, 0, 0, 0.8)";
 }
+
+
