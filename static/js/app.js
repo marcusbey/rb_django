f4fb7d342d0978eeda29a44ee62d6381c0f8eada
Romain BOBOE <rboboe@gmail.com>
2020-07-11 00:00:16 -0400

    building about page, section filed

--- static/js/app.js
+++ static/js/app.js
@@ -34,7 +34,31 @@ var closeNav = () => {
   pathLogo.style.stroke = "rgba(255, 255, 255, 0.8)" :
   pathLogo.style.stroke = "rgba(0, 0, 0, 0.8)";
   fixedTop.style.zIndex = 1030;
-}
+};
 
 
 // Add evel listiner to the header.
+const url = "../img/about/bg-programming.jpg"
+const controller = new ScrollMagic.Controller();
+const controller2 = new ScrollMagic.Controller();
+
+const changeBackGround = new ScrollMagic.Scene({
+  triggerElement: ".float-box",
+  triggerHook: 0.9
+})
+  .addIndicators({colorStart: 'blue', colorTrigger: "blue"})
+  .on('start', function(){
+    $('.leading').toggleClass('hide');
+  })
+  .setClassToggle('.about-header','bg-programming')
+  .addTo(controller);
+
+const fixedFielHeader = new ScrollMagic.Scene({
+  triggerElement: ".about-field",
+  triggerHook: 0.09
+})
+  .addIndicators({colorStart: 'orange', colorTrigger: "orange"})
+  .on('start', function(){
+    $('.leading-field').toggleClass('about-field-fixed')
+  })
+  .addTo(controller2);
