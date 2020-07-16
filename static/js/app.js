cafc8dcde4ff55fad07d4311152f2a1ba23f8806
Romain BOBOE <rboboe@gmail.com>
2020-07-15 22:45:16 -0400

    changin the home and the about page

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,16 @@
-var mySidePage = document.getElementById("mySidenav");
-var main = document.getElementById("main");
-var pathLogo = document.getElementById("path");
-var mainNav = document.getElementById("mainNav");
-var fixedTop = document.querySelector(".fixed-top");
+const mySidePage = document.getElementById("mySidenav");
+const main = document.getElementById("main");
+const pathLogo = document.getElementById("path");
+const mainNav = document.getElementById("mainNav");
+const fixedTop = document.querySelector(".fixed-top");
 
 function  fixed(e){
-  var element = document.querySelector(e);
+  const element = document.querySelector(e);
   element.style.position = 'fixed';
 }
 
-var openNav =()=> {
-  var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+const openNav =()=> {
+  const screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
   if (screenWidth < 768) {
     mySidePage.style.height = "50vh";
     main.style.marginTop = "50vh";
@@ -26,7 +26,7 @@ var openNav =()=> {
 
 };
 
-var closeNav = () => {
+const closeNav = () => {
   mySidePage.style.width = "0";
   main.style.marginLeft= "0";
 
@@ -39,26 +39,71 @@ var closeNav = () => {
 
 // Add evel listiner to the header.
 const url = "../img/about/bg-programming.jpg"
-const controller = new ScrollMagic.Controller();
+const controller0 = new ScrollMagic.Controller();
+const controller1 = new ScrollMagic.Controller();
 const controller2 = new ScrollMagic.Controller();
+const controller3 = new ScrollMagic.Controller();
 
 const changeBackGround = new ScrollMagic.Scene({
-  triggerElement: ".float-box",
-  triggerHook: 0.9
+  triggerElement: ".about-intro",
+  triggerHook: 0.8,
+  reverse: true
 })
   .addIndicators({colorStart: 'blue', colorTrigger: "blue"})
   .on('start', function(){
-    $('.leading').toggleClass('hide');
-  })
-  .setClassToggle('.about-header','bg-programming')
-  .addTo(controller);
+      $(window).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+        $('.leading').css({
+          opacity: function() {
+            var elementHeight = $(this).height();
+            while (scrollTop < elementHeight - 10)
+             {
+              return (elementHeight - scrollTop) / elementHeight;
+            }
+          }
+        });
+      });
+    })
+ // .setClassToggle('.about-header','bg-programming')
+  .addTo(controller0);
 
-const fixedFielHeader = new ScrollMagic.Scene({
-  triggerElement: ".about-field",
+
+const fixedsoftHeader = new ScrollMagic.Scene({
+  triggerElement: ".head-soft",
   triggerHook: 0.09
 })
   .addIndicators({colorStart: 'orange', colorTrigger: "orange"})
   .on('start', function(){
-    $('.leading-field').toggleClass('about-field-fixed')
+    $('.head-soft').toggleClass('about-fixed')
   })
   .addTo(controller2);
+
+const fixedarchHeader = new ScrollMagic.Scene({
+  triggerElement: ".head-design",
+  triggerHook: 0.09
+})
+  .addIndicators({colorStart: 'green', colorTrigger: "green"})
+  .on('start', function(){
+    $('.head-design').toggleClass('about-fixed')
+  })
+  .addTo(controller3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
